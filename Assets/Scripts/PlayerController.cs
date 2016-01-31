9c590a159b4031ca6e9e7e561ff35092ff27a700
Sandeep Kumar Pilakhnawal <sandeep.pilakhnawal@gmail.com>
2016-01-31 23:57:52 +0530

    Added scripts to collect Pick-ups, display score and ending the game.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour {
-	
+public class PlayerController : MonoBehaviour
+{
+	public Text countText;
+	public Text winText;
 	private Rigidbody rb;
-	public float speed =10;
+	public float speed = 10;
+	private int count = 0;
 
-	void Start(){
+	void Start ()
+	{
 		rb = GetComponent<Rigidbody> ();
+		SetCountText ();
+		winText.text = " ";
 	}
-	void FixedUpdate () {
+
+	void FixedUpdate ()
+	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		rb.AddForce (movement*speed);
+		rb.AddForce (movement * speed);
 	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			count++;
+			SetCountText ();
+			other.gameObject.SetActive (false);
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Count: = " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "You Win";
+		}
+	}
+
 }
